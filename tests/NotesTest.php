7fb4961840f94cc908307ec870ae835d25376eea
trapzpro <trapzpro@users.noreply.github.com>
2022-02-20 01:56:02 +0000

    Fix styling

--- tests/NotesTest.php
+++ tests/NotesTest.php
@@ -1,12 +1,11 @@
 <?php
 
-use Trapzpro\QuickLogger\Note;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Trapzpro\QuickLogger\Note;
 
 uses(RefreshDatabase::class);
 
 it('can create a note', function () {
-
     $note = Note::factory()->create();
 
     $this->assertCount(1, Note::all());
